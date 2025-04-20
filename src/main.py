bfa871552af8a221d961ea18e2e2e778b6e05d8d
Diego <91valenciadiego@gmail.com>
2025-04-20 00:07:40 -0500

    Implementación de la reserva en el menú principal

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
+from datetime import datetime
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))  # Añade la raíz al path
 from models.huesped import Huesped
 from utils.manager_huesped import ManagerHuesped
 from logs.registro_logs import RegistroLogs
+from models.reservas import Reservas
+from utils.manager_reservas import ManagerReservas
 
 manager_huesped = ManagerHuesped()
+manager_reservas = ManagerReservas()
 registro_logs = RegistroLogs()
 
 
@@ -13,8 +17,9 @@ registro_logs = RegistroLogs()
 while True:
     print("\n1. Registrar Huesped")
     print("2. Mostrar Huespedes")
-    
-    print("3. Salir")
+    print("3. Registrar Reserva")
+    print("4. Mostrar Reservas")
+    print("5. Salir")
     opcion = input("Seleccione una opción: ")
 
     if opcion == "1":
@@ -33,6 +38,31 @@ while True:
             manager_huesped.__str__()
 
     elif opcion == "3":
+        id_reserva = int(input("Ingrese el ID de la reserva: "))
+        id_cliente = int(input("Ingrese el ID del cliente: "))
+        id_habitacion = int(input("Ingrese el ID de la habitación: "))
+        y_inicio = input("Ingrese la año de reserva (YYYY): ").strip()
+        m_inicio = input("Ingrese la año de reserva (mm): ").strip()
+        d_inicio = input("Ingrese la año de reserva (dd): ").strip()
+
+        fecha_inicio = datetime(int(y_inicio), int(m_inicio), int(d_inicio))
+
+        dias_reserva = int(input("Ingrese la cantidad de días de reserva: "))
+
+        nueva_reserva = Reservas(id_reserva, id_cliente, id_habitacion, fecha_inicio, dias_reserva)
+        manager_reservas.nuevo_registro(nueva_reserva.__to_dict__())
+        registro_logs.registrar(f"Se ha registrado la reserva: {nueva_reserva}")
+
+        manager_reservas.guardar_reservas()
+        print(f"Reserva registrada: {nueva_reserva}")        
+
+    elif opcion == "4":
+        manager_reservas.__str__()
+        #print(manager_reservas.reservas)
+
+    elif opcion == "5":
+        print("Saliendo del programa...")
+        registro_logs.registrar("El programa ha sido cerrado.")
         break
 
     else:
--- src/models/reservas.py
+++ src/models/reservas.py
@@ -11,14 +11,14 @@ class Reservas:
         self.fecha_fin = self.fecha_inicio + timedelta(days=self.dias_reserva)
 
     def __str__(self):
-        return f"Reserva {self.id_reserva}: Cliente {self.id_cliente}, Habitación {self.id_habitacion}, Desde {self.fecha_inicio} hasta {self.fecha_fin}"
+        return f"Reserva {self.id_reserva}: Cliente {self.id_cliente}, Habitación {self.id_habitacion}, Fecha de inicio {self.fecha_inicio.strftime('%Y-%m-%d')}, Días de reserva {self.dias_reserva}, Fecha de fin {self.fecha_fin.strftime('%Y-%m-%d')}"
     
     def __to_dict__(self):
         return {
             'id_reserva': self.id_reserva,
             'id_cliente': self.id_cliente,
             'id_habitacion': self.id_habitacion,
-            'fecha_inicio': self.fecha_inicio,
+            'fecha_inicio': self.fecha_inicio.strftime('%Y-%m-%d'),
             'dias_reserva': self.dias_reserva,
-            'fecha_fin': self.fecha_fin
+            'fecha_fin': self.fecha_fin.strftime('%Y-%m-%d'),
         }
