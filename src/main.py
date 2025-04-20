a9356f3fd4cc93653dca2c578914975fcfaad065
Diego <91valenciadiego@gmail.com>
2025-04-19 22:02:46 -0500

    Implementación menu principal

---
+++ src/main.py
@@ -0,0 +1,40 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))  # Añade la raíz al path
+from models.huesped import Huesped
+from utils.manager_huesped import ManagerHuesped
+from logs.registro_logs import RegistroLogs
+
+manager_huesped = ManagerHuesped()
+registro_logs = RegistroLogs()
+
+
+
+while True:
+    print("1. Registrar Huesped")
+    print("2. Mostrar Huespedes")
+    print("3. Salir")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        id_huesped = int(input("Ingrese el ID del huesped: "))
+        nombre = input("Ingrese el nombre del huesped: ")
+        correo = input("Ingrese el correo del huesped: ")
+        telefono = input("Ingrese el teléfono del huesped: ")
+
+        nuevo_huesped = Huesped(id_huesped, nombre, correo, telefono)
+        manager_huesped.nuevo_registro(nuevo_huesped.__to_dict__())
+        registro_logs.registrar(f"Se ha registrado el huesped: {nuevo_huesped}")
+        manager_huesped.guardar_huespedes()
+        print(f"Huesped registrado: {nuevo_huesped}")
+
+    elif opcion == "2":
+        for huesped in manager_huesped.huespedes:
+            print(huesped)
+
+    elif opcion == "3":
+        break
+
+    else:
+        print("Opción no válida. Intente nuevamente.")
+# from logs.registro_logs import RegistroLogs
